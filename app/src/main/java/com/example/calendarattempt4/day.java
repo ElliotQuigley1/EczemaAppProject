546ff5003cef7abb023614610bf5fb6119aad84b
ElliotQuigley1 <elliot.quigley17@imperial.ac.uk>
2020-01-10 16:13:49 +0000

    update#4

--- app/src/main/java/com/example/calendarattempt4/day.java
+++ app/src/main/java/com/example/calendarattempt4/day.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 
 public class day {
     // Initialises objects
-    protected int Day_ID;               //Stores DAY ID, unique, used for database search
+    private int Day_ID;               //Stores DAY ID, unique, used for database search
     protected int parent_ID;            //Stores PARENT ID for each CHILD's entry
     protected int child_ID;             //Stores CHILD ID for each entry
     protected String date;              //Stores actual date as a string
@@ -15,7 +15,18 @@ public class day {
     // Statement to connect to Postgresql
     protected Statement s;
 
+
+
     public day(){}
+
+    public void setDay_ID(int did) {
+        this.Day_ID = did;
+    }
+
+
+    public int getDay_ID() {
+        return Day_ID;
+    }
 
     //Checking if date entry already exists
     // Passes on Statement s to day and retrieve data for selected day
@@ -29,7 +40,7 @@ public class day {
         String sqlStr = "SELECT * FROM dates WHERE CID =\'"+CID+"\' and date =\'"+date+"\';";
         ResultSet rset=s.executeQuery(sqlStr);
         while(rset.next()) {
-            this.Day_ID = rset.getInt("DID");
+            setDay_ID(rset.getInt("DID"));
             this.answers = rset.getString("Record");
             AUTH = rset.getString("DID");
         }
